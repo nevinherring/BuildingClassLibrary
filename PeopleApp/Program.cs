d423430235b0a1940bfcda873b0b954292883591
Nevin Herring <nevin.herring@gmail.com>
2021-03-02 21:24:12 -0600

    added new BankAccount class

--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -44,6 +44,21 @@ namespace PeopleApp
                 arg0: alice.Name,
                 arg1: alice.DateOfBirth
             );
+            BankAccount.InterestRate = 0.012M; // store a shared value
+            var jonesAccount = new BankAccount();
+            jonesAccount.AccountName = "Mrs. Jones";
+            jonesAccount.Balance = 2400;
+            WriteLine(format: "{0} earned {1:C} interest.",
+            arg0: jonesAccount.AccountName,
+            arg1: jonesAccount.Balance * BankAccount.InterestRate
+            );
+            var gerrierAccount = new BankAccount();
+            gerrierAccount.AccountName = "Ms. Gerrier";
+            gerrierAccount.Balance = 98;
+            WriteLine(format: "{0} earned {1:C} interest",
+            arg0: gerrierAccount.AccountName,
+            arg1: gerrierAccount.Balance * BankAccount.InterestRate
+            );
         }
     }
 }
